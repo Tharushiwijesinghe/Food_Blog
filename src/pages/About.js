1e0e5558e35766597de922d548765a4bc3be8478
Tharushiwijesinghe <tharushiwijesinghe99@gmail.com>
2025-07-26 02:50:21 +0530

    update responsiveness of mobile view

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,27 +13,29 @@ const About = () => {
       <div className="row align-items-center about-container">
         <div className="col-md-6 ">
           <h2 className="mb-3">Welcome to LankanTast 🇱🇰</h2>
-          <p>
-            At <strong>LankanTast</strong>, we're passionate about bringing the vibrant flavors of Sri Lanka
-            to your table. Whether you're a home cook, food lover, or curious traveler, we offer an authentic taste
-            of Sri Lankan cuisine through rich stories, traditional recipes, and cultural insights.
-          </p>
+          <div className='about-description'>
+            <p>
+              At <strong>LankanTast</strong>, we're passionate about bringing the vibrant flavors of Sri Lanka
+              to your table. Whether you're a home cook, food lover, or curious traveler, we offer an authentic taste
+              of Sri Lankan cuisine through rich stories, traditional recipes, and cultural insights.
+            </p>
 
-          <p>
-            Our blog features dishes from every corner of the island — from spicy curries and hoppers to sweet treats like
-            kavum and kokis. We believe food connects people and tells stories passed down through generations.
-          </p>
+            <p>
+              Our blog features dishes from every corner of the island — from spicy curries and hoppers to sweet treats like
+              kavum and kokis. We believe food connects people and tells stories passed down through generations.
+            </p>
 
-          <h5 className="mt-4 text-warning">🍛 What We Offer:</h5>
-          <ul>
-            <li>Authentic Sri Lankan recipes with step-by-step guides</li>
-            <li>Fusion dishes and creative twists</li>
-            <li>Cooking tips, kitchen hacks, and spice knowledge</li>
-          </ul>
+            <h5 className="mt-4 text-warning">🍛 What We Offer:</h5>
+            <ul>
+              <li>Authentic Sri Lankan recipes with step-by-step guides</li>
+              <li>Fusion dishes and creative twists</li>
+              <li>Cooking tips, kitchen hacks, and spice knowledge</li>
+            </ul>
 
-          <p className="mt-4 font-weight-bold">
-            Thank you for being part of the LankanTast journey!
-          </p>
+            <p className="mt-4 font-weight-bold">
+              Thank you for being part of the LankanTast journey!
+            </p>
+          </div>
         </div>
 
         <div className="col-md-6 text-center mt-4 mt-md-0">
